eb2c5f20a27cd71a3fad429343aba0fadb1ac50e
Kiril Popovski <48929770+KirilPopovski@users.noreply.github.com>
2020-07-19 09:40:07 +0300

    Add register structure for client

--- Client/HotelSystemClient/src/app/app.module.ts
+++ Client/HotelSystemClient/src/app/app.module.ts
@@ -1,22 +1,28 @@
+import { UserService } from './user.service';
 import { ReservationsService } from './reservations/reservations.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ReservationsComponent } from './reservations/reservations.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms'
+import { RegisterComponent } from './register/register.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReservationsComponent
+    ReservationsComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
-    ReservationsService
+    ReservationsService,
+    UserService
   ],
   bootstrap: [AppComponent]
 })
